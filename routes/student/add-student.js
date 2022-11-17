f31278cdf9d184b0a8c95e52561bfa9b72890a91
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2022-11-17 14:23:19 +0530

    added the encryption and decryption

--- routes/student/add-student.js
+++ routes/student/add-student.js
@@ -7,10 +7,25 @@ cloudinary.config({
     api_secret: 'VrE5wG2lYuobc0S5atbZPe3PhO4',
     secure: true
 });
+const encryption = (str) => {
+    let encr = "";
+    for(let i=0;i<str.length; i++){
+        if(i%2 == 0){
+            encr = encr + str[i]
+        }
+    }
+    for(let i=0;i<str.length; i++){
+        if(i%2 == 1){
+            encr = encr + str[i]
+        }
+    }
+    return encr
+}
 const addStudent = (req, res) => {
     const reg_num = req.body.reg_num;
     const name = req.body.name;
     const img = req.files.img;
+
     cloudinary.uploader.upload(img.tempFilePath, (err, rslt) => {
         if(err){
             res.send(err.message);
@@ -18,7 +33,7 @@ const addStudent = (req, res) => {
         else{
             let studentm = new studentModel();
             studentm.name = name;
-            studentm.registration_num = reg_num;
+            studentm.registration_num = encryption(reg_num);
             studentm.url = rslt.url
             studentm.save((err, data) => {
                 if(err){
--- routes/student/verify-student.js
+++ routes/student/verify-student.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const studentModel = require('../../model/student');
-
+const decryption = (str) => {
+    let encr = "";
+    for(let i=0;i<str.length; i++){
+        if(i%2 == 0){
+            encr = encr + str[i]
+        }
+    }
+    for(let i=0;i<str.length; i++){
+        if(i%2 == 1){
+            encr = encr + str[i]
+        }
+    }
+    return encr
+}
 const verifyStudent = (req, res) => {
-    const reg_num = req.body.reg_num;
+    const reg_num = decryption(req.body.reg_num);
     studentModel.find({
         registration_num: reg_num
     }).then((response) => {
