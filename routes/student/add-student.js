91fba8c1eb085a0b650bdd2e14f0039078439d86
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2022-11-19 12:04:11 +0530

    updated the verify student module

--- routes/student/add-student.js
+++ routes/student/add-student.js
@@ -7,25 +7,35 @@ cloudinary.config({
     api_secret: 'VrE5wG2lYuobc0S5atbZPe3PhO4',
     secure: true
 });
-const encryption = (str) => {
-    let encr = "";
-    for(let i=0;i<str.length; i++){
-        if(i%2 == 0){
-            encr = encr + str[i]
-        }
-    }
-    for(let i=0;i<str.length; i++){
-        if(i%2 == 1){
-            encr = encr + str[i]
-        }
-    }
-    return encr
-}
+// const encryption = (str) => {
+//     let encr = "";
+//     for(let i=0;i<str.length(); i++){
+//         if(i%2 == 0){
+//             encr = encr + str[i]
+//         }
+//     }
+//     for(let i=0;i<str.length(); i++){
+//         if(i%2 == 1){
+//             encr = encr + str[i]
+//         }
+//     }
+//     return encr
+// }
 const addStudent = (req, res) => {
     const reg_num = req.body.reg_num;
     const name = req.body.name;
     const img = req.files.img;
-
+    let encr = "";
+    for(let i=0;i<reg_num.length; i++){
+        if(i%2 == 0){
+            encr = encr + reg_num[i]
+        }
+    }
+    for(let i=0;i<reg_num.length; i++){
+        if(i%2 == 1){
+            encr = encr + reg_num[i]
+        }
+    }
     cloudinary.uploader.upload(img.tempFilePath, (err, rslt) => {
         if(err){
             res.send(err.message);
@@ -33,8 +43,9 @@ const addStudent = (req, res) => {
         else{
             let studentm = new studentModel();
             studentm.name = name;
-            studentm.registration_num = encryption(reg_num);
+            studentm.registration_num = encr;
             studentm.url = rslt.url
+            // studentModel.reg = encr
             studentm.save((err, data) => {
                 if(err){
                     res.send(err.message);
